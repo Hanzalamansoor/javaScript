fdb2380bf118814c777e32784514fbe61d4dce0a
Hanzalamansoor <145720880+Hanzalamansoor@users.noreply.github.com>
2024-10-12 21:09:23 +0500

    Create node.js

---
+++ node.js
@@ -0,0 +1,33 @@
+// let a = 45
+// console.log(a)
+// a = "harry"
+// console.log(a)
+
+//before es6(ECMA Script is modern javaScript) programmers used to use var to define a variable 
+//const is used to decleare the constant numbers that us unchangeable 
+//Let is used for blocked scope variable 
+
+// blocked means {}
+
+let b = "Harry"
+{
+    let b = 45
+    console.log(b)
+}
+
+console.log(b)
+
+
+
+let c = 45
+c = "Ahmed" //45 replaced by Ahmed
+console.log(c)
+
+
+//Now const can not be changed 
+
+const author = "Harry"
+
+
+
+
