4ca26b6d6ca10bab50187f9bdfc37f937cbcabcd
Hanzalamansoor <145720880+Hanzalamansoor@users.noreply.github.com>
2024-10-12 21:12:46 +0500

    Create chp1.js

---
+++ chp1.js
@@ -0,0 +1,39 @@
+//Create a variable of type string and add number to it
+
+// let a = "harry"
+// let b = 50
+// console.log(a + b)
+
+
+// //chapter 1 - q2
+// console.log(typeof(a+b))
+
+
+//chapter 1 - q3
+// const c = {
+//     name : "Harry",
+//     section : 1,
+//     isPrincipal: false
+// }
+
+// c['firend'] = "Daniyal"
+// c['name'] = "zakir"  //alt+shift down arrow to replicate the first line
+// console.log(c)
+
+// c = {name : "ali", section : 1}
+//the upper code will throw an error
+//Now object inside of const can change 
+
+//chapter 1 - q5
+
+const dict = {
+    appreciate : "recognize the full worth of.",
+    bureaucracy : "a system of government in which most of the important decisions are taken by state officials rather than by elected representatives.",
+    contemporary : "living or occurring at the same time.",
+    diversity : "the state of being diverse; variety.",
+    efficacy : "the ability to produce a desired or intended result."
+}
+
+console.log(dict.bureaucracy)
+
+
